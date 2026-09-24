09fc674b938b6de64a65fe03474ba5896bc00548
iijietka <iijietka@users.noreply.github.com>
2026-09-24 15:20:39 +1200

    Изменения в MyClass

--- MyClass.java
+++ MyClass.java
@@ -2,5 +2,7 @@ java
 public class MyClass {
     public static void main(String[] args) {
         System.out.println("Hello, GitHub!");
+        System.out.println("Изменение от iijietka");
+        System.out.println("Версия 2");
     }
 }
